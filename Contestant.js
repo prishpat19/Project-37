f74d5b78a862d2f14d6ca37e440f1571840e1bfc
prishpat19 <72462997+prishpat19@users.noreply.github.com>
2021-07-25 13:10:24 -0400

    Add files via upload

---
+++ Contestant.js
@@ -0,0 +1,38 @@
+class Contestant{
+
+    constructor(){
+        this.index = null;
+        this.answer = 0;
+        this.name = null;
+    }
+
+    getCount(){
+        var contestantCountRef = database.ref('contestantCount');
+        contestantCountRef.on("value", (data) =>{
+            contestantCount = data.val();
+        
+        })
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+            contestantCount: count
+        });
+    }
+
+    update(){
+        var contestantIndex = "contestants/contestant" + this.index;
+        database.ref(contestantindex).set({
+            name: this.name,
+            answer: this.answer
+        });
+    }
+
+    static getPlayerInfo(){
+        var contestantInforef = database.ref('contestants');
+        contestantsInforef.on("value", (data)=>{
+            allContestants = data.val();
+        })
+    }
+    
+}
